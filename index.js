24e84cfd96765badd51fad3f73fe6e87e292785d
Javier Cobos <javiercobosmesa@gmail.com>
2020-02-20 14:06:22 +0100

    fixes

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = function mimimize(text) {
-  text.replace(/a|e|o|u/g, "i").replace(/A|I|O|U/g, "I");
+  return text.replace(/a|e|o|u/g, "i").replace(/A|I|O|U/g, "I");
 };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-var mimimize = require("./mimimize");
+var mimimize = require("./index.js");
 
 var testSuite = {
   One: "Ini",
@@ -7,6 +7,7 @@ var testSuite = {
   "One day I'll mimimize the world": "Ini diy I'll mimimizi thi wirld",
   "This 123 is number 123 stuff 912": "This 123 is nimbir 123 stiff 912"
 };
+
 test("Mimimizes", () => {
   for (var k in testSuite) {
     if (testSuite.hasOwnProperty(k)) {
