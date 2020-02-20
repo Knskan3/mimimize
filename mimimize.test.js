1d3a901e8d3ae3368d05cabd9e739680c00062e8
Javier Cobos <javiercobosmesa@gmail.com>
2020-02-20 13:29:34 +0100

    tests update

--- mimimize.test.js
+++ mimimize.test.js
@@ -1,15 +1,16 @@
 var mimimize = require("./mimimize");
 
 var testSuite = {
-	One: "Ini",
-	day: "diy",
-	I: "I",
-	"One day I'll mimimize the world": "Ini diy I'll mimimizi thi wirld"
+  One: "Ini",
+  day: "diy",
+  I: "I",
+  "One day I'll mimimize the world": "Ini diy I'll mimimizi thi wirld",
+  "This 123 is number 123 stuff 912": "This 123 is nimbir 123 stiff 912"
 };
 test("Mimimizes", () => {
-	for (var k in testSuite) {
-		if (testSuite.hasOwnProperty(k)) {
-			expect(mimimize(k)).toEqual(testSuite[k]);
-		}
-	}
+  for (var k in testSuite) {
+    if (testSuite.hasOwnProperty(k)) {
+      expect(mimimize(k)).toEqual(testSuite[k]);
+    }
+  }
 });
